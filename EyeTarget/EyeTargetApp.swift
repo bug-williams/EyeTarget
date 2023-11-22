8dffa4a9bc82d2f07b8faa69ff1f125a32c3c99d
Bug Williams <zachwilliams.services@gmail.com>
2023-11-22 18:02:48 -0500

    Initial Commit

---
+++ EyeTarget/EyeTargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeTargetApp.swift
+//  EyeTarget
+//
+//  Created by Bug on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct EyeTargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
